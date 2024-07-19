7664ad8f129e7871b4911a6c21f323ab2f6b7120
Gospel Darlington <darlingtongospel@gmail.com>
2024-07-19 11:14:06 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  defaultNetwork: 'localhost',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+  },
+};
